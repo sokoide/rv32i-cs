52f7d5cd46e2b705ad048cfd835e16fec0eba3cc
So Koide <sokoide@gmail.cojm>
2024-06-02 17:38:43 +0900

    add: fib(30) example

--- Program.cs
+++ Program.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using System.IO;
-using NLog;
+﻿using NLog;
+using NDesk.Options;
 using NLog.Config;
+using System.Diagnostics;
 
 public class Program
 {
     private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-    public static void Main()
+    static bool showHelp = false;
+    static bool demo = false;
+    static string loglevel = "info";
+    static string sourcePath = "";
+    static string end = "";
+
+    public static void Help(OptionSet p)
     {
-        LogManager.Configuration = new LoggingConfiguration();
-        LogManager.Configuration.LoggingRules.Clear();
-        LogManager.Configuration.LoggingRules.Add(new LoggingRule("*", LogLevel.Info, new NLog.Targets.ConsoleTarget()));
-        // LogManager.Configuration.LoggingRules.Add(new LoggingRule("*", LogLevel.Trace, new NLog.Targets.ConsoleTarget()));
-        LogManager.ReconfigExistingLoggers();
+        Console.WriteLine("Usage: dotnet run [OPTIONS]");
+        Console.WriteLine();
+        Console.WriteLine("Options:");
+        p.WriteOptionDescriptions(Console.Out);
+    }
 
-        logger.Info("* Started");
-        logger.Info("* Running a small program");
-
+    public static void Demo()
+    {
         var emu = new RV32I.Emulator();
-
         emu.Reset();
 
         // test binary in sample-binary-001.txt
@@ -59,6 +63,87 @@ public class Program
         emu.StepUntil(0x18);
         emu.Dump();
         emu.StepUntil(0x1c);
+    }
+
+    public static void Run(string sourcePath, uint end)
+    {
+        var emu = new RV32I.Emulator();
+        emu.Reset();
+
+        logger.Info($"* Loading {sourcePath}");
+        emu.Load(sourcePath);
+
+        logger.Info($"* Running until 0x{end:X8}");
+        Stopwatch sw = new Stopwatch();
+        sw.Reset();
+        sw.Start();
+        emu.StepUntil(end);
+        sw.Stop();
+
+        emu.Dump();
+        long t = sw.ElapsedMilliseconds;
+        logger.Info("* Elapsed time: {0} ms", t);
+    }
+
+    public static void Main(string[] args)
+    {
+        var p = new OptionSet() {
+            { "l|loglevel=", "loglevel: info, trace, debug, warn, error", v => loglevel = v },
+            { "s|sourcepath=", "source file path", v => sourcePath = v },
+            { "e|end=", "address of the source to stop execution", v => end = v },
+            { "d|demo",  "run a demo program", v => demo = v != null },
+            { "h|help",  "show help", v => showHelp = v != null },
+        };
+        List<string> extra;
+        try
+        {
+            extra = p.Parse(args);
+        }
+        catch (OptionException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Try `dotnet run -- --help' for more information.");
+            return;
+        }
+
+        if (showHelp)
+        {
+            Help(p);
+            return;
+        }
+
+        LogManager.Configuration = new LoggingConfiguration();
+        LogManager.Configuration.LoggingRules.Clear();
+
+        var ll = LogLevel.Info;
+        if (loglevel.Length > 0)
+        {
+            ll = LogLevel.FromString(loglevel);
+        }
+
+        LogManager.Configuration.LoggingRules.Add(new LoggingRule("*", ll, new NLog.Targets.ConsoleTarget()));
+        LogManager.ReconfigExistingLoggers();
+
+        logger.Info("* Started");
+        logger.Info("* Running a small program");
+
+        if (demo)
+        {
+            Demo();
+        }
+        else
+        {
+            uint uintEnd;
+            if (end.StartsWith("0x"))
+            {
+                uintEnd = Convert.ToUInt32(end, 16);
+            }
+            else
+            {
+                uintEnd = Convert.ToUInt32(end, 10);
+            }
+            Run(sourcePath, uintEnd);
+        }
 
         logger.Info("* Completed");
     }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RV32I emulator in C#
+# RV32I emulator in C #
 
 ## About
 
@@ -7,8 +7,22 @@
 ## How to run
 
 ```
-dotnet run
+dotnet run --project rv32i-cs.csproj -- --demo
+dotnet run --project rv32i-cs.csproj -- -s ./data/sample-binary-001.txt -e 0x0c -l trace
+dotnet run --project rv32i-cs.csproj -- -s ./data/sample-binary-001.txt -e 0x0c -l trace
+# .net 8 test
+dotnet run --framework net8.0 --configuration Release --project rv32i-cs.csproj --  -s ./data/sample-binary-fib.txt -e 0x130
+# .net 6 test
+dotnet run --framework net6.0 --configuration Release --project rv32i-cs-net6.csproj -- -s ./data/sample-binary-fib.txt -e 0x130
+
+2024-06-02 17:32:46.6467|INFO|rv32i-cs.Program|* Elapsed time: 17215 ms
 ```
+
+## Quick Benchmark on M1 Mac mini
+
+* rv32i-go built by go1.22.3:  3.4s
+* rv32i-cs on .net 8.0.6:     17.2s
+* rv32i-cs on .net 6.0.31:    32.3s
 
 ## Note
 
